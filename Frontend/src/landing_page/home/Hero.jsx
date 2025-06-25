dc31f8088e7aec8c14f873b600f6a8d4bbcaf75b
om <omjha003@gmail.com>
2025-06-25 14:07:35 +0530

    Done with hero and awards component

--- Frontend/src/landing_page/home/Hero.jsx
+++ Frontend/src/landing_page/home/Hero.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 export default function Hero() {
   return (
     <div>
-      Hero
+      <div className="container">
+          <div className="row text-center">
+              <img src="media/images/homeHero.png" alt="Hero Image" className="mb-5" />
+              <h1>Invest in Everything</h1>
+              <p style={{margin : "5px 0px 15px 0px"}}>Online platform in stocks, derivatives , mutual funds and more</p>
+              <button className='p-2 btn btn-primary fs-7' style={{width : "17%" , margin : "0 auto"}}>Signup Now</button>
+          </div>
+      </div>
+
     </div>
   )
 }
