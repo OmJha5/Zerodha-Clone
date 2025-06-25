8ef970b1c2625864337eadec5c3abd798f6dc291
om <omjha003@gmail.com>
2025-06-25 13:22:30 +0530

    Added all images for the project and integrated bootstrap and font awesome to the project

--- Frontend/src/landing_page/Navbar.jsx
+++ Frontend/src/landing_page/Navbar.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-export default function OpenAccount() {
+export default function Navbar() {
   return (
     <div>
-        OpenAccont
+        Navbar
     </div>
   )
 }
