f6ae93316e95d376f34c5b0b011158cea8cd2d2d
om <omjha003@gmail.com>
2025-07-12 16:19:24 +0530

    Setup Holdings and positions section

--- dashboard/src/components/Holdings.jsx
+++ dashboard/src/components/Holdings.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { holdings } from '../data/data'
 
 export default function Holdings() {
   return (
     <div>
-      <h3 className="title">Holdings (13)</h3>
+      <h3 className="title">Holdings ({holdings.length})</h3>
 
       <div className="order-table">
         <table>
@@ -17,6 +18,26 @@ export default function Holdings() {
             <th>Net chg.</th>
             <th>Day chg.</th>
           </tr>
+
+          {holdings.map((stock , ind) => {
+              const currValue = stock.price * stock.qty;
+              const isProfit = currValue - (stock.avg * stock.qty) >= 0.0;
+              const profClass = isProfit ? "profit" : "loss";
+              const dayClass = stock.isLoss ? "loss" : "profit";
+              
+              return <tr key={ind}>
+                  <td>{stock.name}</td>
+                  <td>{stock.qty}</td>
+                  <td>{stock.avg.toFixed(2)}</td>
+                  <td>{stock.price.toFixed(2)}</td>
+                  <td>{currValue.toFixed(2)}</td>
+                  <td className={profClass}>{(currValue - (stock.avg * stock.qty)).toFixed(2)}</td>
+                  <td className={profClass}>{stock.net}</td>
+                  <td className={dayClass}>{stock.day}</td>
+
+              </tr>
+          })}
+
         </table>
       </div>
 
