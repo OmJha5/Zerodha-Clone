2d69f4d38f45846d40ed6593a23ef782c5ec5a37
om <omjha003@gmail.com>
2025-07-16 00:49:25 +0530

    fix: add USER_API_ENDPOINT export

--- dashboard/utils/apiendpoint.js
+++ dashboard/utils/apiendpoint.js
@@ -4,3 +4,4 @@ export let HOLDING_API_ENDPOINT  = "https://zerodha-clone-backend-bxss.onrender.
 export let ORDER_API_ENDPOINT  = "https://zerodha-clone-backend-bxss.onrender.com/api/order"
 export let FUNDS_API_ENDPOINT  = "https://zerodha-clone-backend-bxss.onrender.com/api/funds"
 export let STOCKS_API_ENDPOINT  = "https://zerodha-clone-backend-bxss.onrender.com/api/stocks"
+export let USER_API_ENDPOINT  = "https://zerodha-clone-backend-bxss.onrender.com/api/user"
